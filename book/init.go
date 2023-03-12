1c8f8f6e61b874bf3b5ca23a6fb95d48e44bcfcc
Tamilselvan R <selvan12@gmail.com>
2023-03-12 13:14:40 -0700

    removed db info

--- book/init.go
+++ book/init.go
@@ -10,8 +10,6 @@ import (
 // InitDB used to init the remote DB (PostgreSQL as a Service)
 func InitDB() *gorm.DB {
 	// Databse info from ElephantDB (PostgreSQL as a Service Cloud Database)
-	// postgres://xhnxbuqe:kGHMnhnc-hbhVZY04JzZP1VUfv_7-OIh@mahmud.db.elephantsql.com/xhnxbuqe
-
 	dsn := "host=mahmud.db.elephantsql.com user=xhnxbuqe password=kGHMnhnc-hbhVZY04JzZP1VUfv_7-OIh dbname=xhnxbuqe port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
